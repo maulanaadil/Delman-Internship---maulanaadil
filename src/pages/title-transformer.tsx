dc7b90548bf9df4c0daf4651aec794dc69ddfdda
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-25 03:04:08 +0700

    change title TitleTransformer page

--- src/pages/title-transformer.tsx
+++ src/pages/title-transformer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { PageWrapper } from '@/layouts';
 
 export const TitleTransformer: NextPage = () => {
-  return <PageWrapper title='Test page'>sadsa</PageWrapper>;
+  return <PageWrapper title='Title Transformer'>hello world</PageWrapper>;
 };
 
 export default TitleTransformer;
