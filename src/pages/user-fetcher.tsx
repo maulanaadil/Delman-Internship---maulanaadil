508fb2d79bbc50c4660fbbe83829edfb321e0829
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-25 03:04:26 +0700

    add UserFetcher page

---
+++ src/pages/user-fetcher.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next';
+import React from 'react';
+import { PageWrapper } from '@/layouts';
+
+export const UserFetcher: NextPage = () => {
+  return <PageWrapper title='User Fetcher'>hello world</PageWrapper>;
+};
+
+export default UserFetcher;
