6ecc665a2ce08e4336ec8b79638ccc7fbe00e122
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-25 03:03:34 +0700

    delete unused page

--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-import React from 'react';
-import { PageWrapper } from '@/layouts';
-
-export const TestPage: NextPage = () => {
-  return <PageWrapper title='Test page'>sadsa</PageWrapper>;
-};
-
-export default TestPage;
