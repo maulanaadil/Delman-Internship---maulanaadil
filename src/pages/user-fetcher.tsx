9071924998d41f94fedbfc3153c4f2377a4dc9e4
Maulana Adil <maulanaadil.latif@gmail.com>
2022-07-26 19:18:57 +0700

    implement feature user fetcher

--- src/pages/user-fetcher.tsx
+++ src/pages/user-fetcher.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from 'next';
 import React from 'react';
 import { PageWrapper } from '@/layouts';
+import UserFetcherModule from '@/modules/user-fetcher';
 
 export const UserFetcher: NextPage = () => {
-  return <PageWrapper title='User Fetcher'>hello world</PageWrapper>;
+  return (
+    <PageWrapper title='User Fetcher'>
+      <UserFetcherModule />
+    </PageWrapper>
+  );
 };
 
 export default UserFetcher;
